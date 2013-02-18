f4be348f50de892697a2743f29881fe04c51311f
Bookram <stephanienkram@gmail.com>
2013-02-18 13:38:49 -0600

    Outlines. Rails initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryRough::Application.config.secret_token = '6f33bf2af95a4baa54cfeae34869c0cf120d369881b2d8af901ea83bd740b34171b1ec4fe0ff6f96d77bc7697529309d7fea1592bbf98f5a0a8d128318b43813'
---
+++ startServer.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rails server --binding=192.168.1.10
